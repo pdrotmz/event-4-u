9e26fde5b7a81e38e7535e9ce82b82b53c2f5ea8
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-09 08:44:38 -0300

    initial commit

---
+++ src/main/java/dev/pdrotmz/event_4_u/Event4UApplication.java
@@ -0,0 +1,13 @@
+package dev.pdrotmz.event_4_u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Event4UApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Event4UApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pdrotmz/event_4_u/Event4UApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pdrotmz.event_4_u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Event4UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
