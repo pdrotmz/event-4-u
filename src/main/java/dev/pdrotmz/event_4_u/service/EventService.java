219f19690b32ee2cdfdf1a149652c69c58b0f37c
pdrotmz <pedrotomaz0210@gmail.com>
2024-09-09 23:00:57 -0300

    feat: add event service

---
+++ src/main/java/dev/pdrotmz/event_4_u/service/EventService.java
@@ -0,0 +1,53 @@
+package dev.pdrotmz.event_4_u.service;
+
+import dev.pdrotmz.event_4_u.domain.Event;
+import dev.pdrotmz.event_4_u.domain.User;
+import dev.pdrotmz.event_4_u.repository.EventRepository;
+import dev.pdrotmz.event_4_u.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final EventRepository repository;
+
+    public EventService(EventRepository repository) {
+        this.repository = repository;
+    }
+
+    public Event createEvent(Event event, String username) {
+        User owner = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("user not Found"));
+        event.setOwner(owner);
+        return repository.save(event);
+    }
+
+    public List<Event> getAllEvents() {
+        return repository.findAll();
+    }
+
+    public Optional<Event> getEventByID(UUID id) {
+        return repository.findById(id);
+    }
+
+    public Event updateEvent(UUID id, Event updateEventInfo) {
+        return repository.findById(id).map(Event -> {
+            Event.setEventTitle(updateEventInfo.getEventTitle());
+            Event.setEventDescription(updateEventInfo.getEventDescription());
+            return repository.save(Event);
+        }).orElseThrow(() -> new EntityNotFoundException("Event not found"));
+    }
+
+    public void deleteEventById(UUID id) {
+        repository.deleteById(id);
+    }
+}
